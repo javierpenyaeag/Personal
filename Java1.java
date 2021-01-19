18479d7996545669033e514bd9979a7f9c69e032
javier <javier.penya.n@gmail.com>
2021-01-19 11:34:38 +0100

    commit de pagina html y hola mundo java

---
+++ Java1.java
@@ -0,0 +1,13 @@
+
+package java1;
+
+public class Java1 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo Sistemas Informaticos");
+        
+    }
+    
+}
